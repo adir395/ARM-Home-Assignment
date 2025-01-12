a5916f62788315ee01425ecfbca8f5a8a34690b3
adir395 <adirmelker1@gmail.com>
2025-01-12 14:17:51 +0200

    vm

--- testTemplate.ps1
+++ testTemplate.ps1
@@ -1,6 +1,11 @@
-$rg = 'AdirMelker-home-assignment'
-New-AzResourceGroup -Name $rg -Location Eastus -Force
+# $rg = 'AdirMelker-home-assignment'
+# New-AzResourceGroup -Name $rg -Location Eastus -Force
 
-New-AzResourceGroupDeployment -Name 'test' -ResourceGroupName $rg -TemplateFile 'testTemplate.json'
+# New-AzResourceGroupDeployment -Name 'testadir33' -ResourceGroupName $rg -TemplateFile 'storagesTemplate.json'
 
-Remove-AzResourceGroup -Name 'AdirMelker-home-assignment' -Force
+# Remove-AzResourceGroup -Name $rg -Force
+
+# Remove-AzStorageAccount -Name 'storageaccountadir1' -Force
+
+
+# New-AzResourceGroupDeployment -Name 'test2' -ResourceGroupName $rg -TemplateFile 'vm1template.json' -TemplateParameterFile 'vmTemplate.parm.json'
